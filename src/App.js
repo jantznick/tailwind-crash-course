21c1fbd0ab47b4ad9bf7173d62e44ca8ac973cc6
Nick Jantz <nick@Rachels-MacBook-Air.local>
2023-12-04 21:29:16 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './app.css'; 
+
+const App = () => {
+	return (
+		<h1 className="text-primary text-4xl font-bold text-center">
+			Hello world! I am using React
+		</h1>
+	)
+}
+
+export default App
